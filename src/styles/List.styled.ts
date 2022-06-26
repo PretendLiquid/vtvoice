e238c9d7406c7fe830356d4b2d523b47a7559a5d
PretendLiquid <pretendliquid@gmail.com>
2022-06-26 13:04:17 +0200

    Change hotkey list item size

--- src/styles/List.styled.ts
+++ src/styles/List.styled.ts
@@ -2,12 +2,8 @@ import styled from "styled-components";
 import "@fontsource/roboto"
 
 export const List = styled.div`
-    background-color: #7948df;
-    border-radius: 25px;
-    padding: 2px;
-
-    height: 100px;
     p {
+        height: 25px;
         color: white;
         font-family: roboto, sans-serif;
         font-weight: bold;
